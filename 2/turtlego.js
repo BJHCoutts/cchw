c1599b4492a1f4228f78fa5fe298a43ee6f15f9c
Brian Coutts <bjhcoutts@gmail.com>
2018-04-29 08:38:36 -0700

    Max arrays fixed, print issues

--- 2/turtlego.js
+++ 2/turtlego.js
@@ -7,13 +7,16 @@ class Turtle {
             'y': y
         };
         this.direction = "e";
-        this.walked = []
-
+        this.walked = [];
+        this.xMax = 0;
+        this.xArray = [];
+        this.yMax = 0;
+        this.yArray = [];
     }
 
 
 
-    right(n) {
+    right() {
 
         if (this.direction === "e") {
             this.direction = "s"
@@ -29,7 +32,7 @@ class Turtle {
         }
         return this
     }
-    left(n) {
+    left() {
 
         if (this.direction === "e") {
             this.direction = "n"
@@ -43,11 +46,9 @@ class Turtle {
             console.log("Invalid direction, resetting to East")
             this.direction = "e"
         }
-        console.log(this.direction)
         return this
     }
 
-    //this.walked =[];
     allPoints(pt) {
         return this.walked.push(pt);
     }
@@ -84,43 +85,32 @@ class Turtle {
         }
         return this;
     }
-
-    maxX(){
-        this.xMax=0;
-        for (let i of this.walked){
-            if (this.walked[i.x]>this.xMax){
-                xMax=i
-            }            
-        }
-        return this.xMax;
-    }
-
-    maxY(){
-        this.yMax=0;
-        for (let i of this.walked){
-            if (this.walked[i.y]>this.yMax){
-                this.yMax=i
-            }            
-        }
-        return this.yMax;
-    }
     
-    // arrayGen() {
-    //     let this.array = [], this.row = [];
-    //     while (this.xMax--) this.row.push(0);
-    //     while (this.yMax--) this.array.push(this.row.slice());
-    //     return this.array;
-    // }
+ 
+      maxX(){
+        for(let i of this.walked){
+          this.xArray.push(i.x)
+        }
+        this.xMax = Math.max(...this.xArray);
+        return this;
+      }
+      
+      maxY(){
+        for(let i of this.walked){
+          this.yArray.push(i.y)
+        }
+        this.yMax = Math.max(...this.yArray);
+        return this;
+      }
 
-    // print(){
-    //         for (let pt of arr){
-    //           board[pt.y][pt.x] = 1;
-    //         }
-    //       }
-    // }
-
-
-
+      print(){
+        this.allPoints(this.location);
+        this.maxX();
+        this.maxY();
+        let myArray = arrayGen(this.xMax,this.yMax);
+        console.log(myArray);
+        return this;
+      }
 }
 
 function arrayGen(width, height) {
@@ -130,27 +120,146 @@ function arrayGen(width, height) {
   return array;
 }
 
-let myArray = arrayGen(3,5)
+const first = new Turtle(0, 0)
 
-const draw = function(arr){
-  for(let i of arr){
-    myArray[i.y][i.x] = 1;
-  }
+first.forward(3).right().forward(2).left().forward(1).print();
+
+
+console.log(first)
+
+class Turtle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.location = {
+            'x': x,
+            'y': y
+        };
+        this.direction = "e";
+        this.walked = [];
+        this.xMax = 0;
+        this.xArray = [];
+        this.yMax = 0;
+        this.yArray = [];
+    }
+
+
+
+    right() {
+
+        if (this.direction === "e") {
+            this.direction = "s"
+        } else if (this.direction === "s") {
+            this.direction = "w"
+        } else if (this.direction === "w") {
+            this.direction = "n"
+        } else if (this.direction === "n") {
+            this.direction = "e"
+        } else {
+            console.log("Invalid direction, resetting to East")
+            this.direction = "e"
+        }
+        return this
+    }
+    left() {
+
+        if (this.direction === "e") {
+            this.direction = "n"
+        } else if (this.direction === "n") {
+            this.direction = "w"
+        } else if (this.direction === "w") {
+            this.direction = "s"
+        } else if (this.direction === "s") {
+            this.direction = "e"
+        } else {
+            console.log("Invalid direction, resetting to East")
+            this.direction = "e"
+        }
+        return this
+    }
+
+    allPoints(pt) {
+        return this.walked.push(pt);
+    }
+
+    forward(steps) {
+        for (let i = 0; i < steps; i++) {
+            this.allPoints(this.location)
+            if (this.direction === "e") {
+                this.location = Object.assign({},
+                    this.location, {
+                        x: this.location['x'] + 1
+                    }
+                )
+            } else if (this.direction === "s") {
+                this.location = Object.assign({},
+                    this.location, {
+                        y: this.location['y'] + 1
+                    }
+                )
+            } else if (this.direction === "w") {
+                this.location = Object.assign({},
+                    this.location, {
+                        x: this.location['x'] - 1
+                    }
+                )
+            } else if (this.direction === "n") {
+                this.location = Object.assign({},
+                    this.location, {
+                        y: this.location['y'] - 1
+
+                    }
+                )
+            }
+        }
+        return this;
+    }
+    
+ 
+      maxX(){
+        for(let i of this.walked){
+          this.xArray.push(i.x)
+        }
+        this.xMax = Math.max(...this.xArray);
+        return this;
+      }
+      
+      maxY(){
+        for(let i of this.walked){
+          this.yArray.push(i.y)
+        }
+        this.yMax = Math.max(...this.yArray);
+        return this;
+      }
+
+      print(){
+        this.allPoints(this.location);
+        this.maxX();
+        this.maxY();
+        let myArray = arrayGen(this.xMax,4);
+        console.log(myArray);
+        for (let x of this.xArray){
+          myArray.push(x);
+        }
+        for (let y of this.yArray){
+          myArray.push(y);
+        }
+        myArry.join(" ")
+        return this;
+      }
+}
+
+function arrayGen(width, height) {
+  let array = [], row = [];
+  while (width--) row.push(0);
+  while (height--) array.push(row.slice());
+  return array;
 }
 
 const first = new Turtle(0, 0)
 
-first.forward(3)
+first.forward(3).right().forward(2).left().forward(1).print();
 
-let a = arrayGen(5,8);
 
-console.log(first);
-
-a;
-
-const xCoord = walked.map(elem => elem.x)
-// returns [x1, x2, x3....]
-// Math.max(...xCoord) // Math.max(x1, x2, x3) //Math.max(3, 6, 2, 9)
-
-redo join array into a string with space
+console.log(first)
 
