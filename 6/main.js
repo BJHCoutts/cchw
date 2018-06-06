bba25f109649fb8446e208a34cb099279c73238e
Brian Coutts <bjhcoutts@gmail.com>
2018-06-05 19:42:15 -0700

    interface updated, final submission

--- 6/main.js
+++ 6/main.js
@@ -1,116 +1,119 @@
-// document.body.onload = addImg()
-document.body.onload = addBlocks()
-// document.body.onload = display()
-
-
-
-let word;
-let allowedGuesses;
-let correctGuesses;
-let wrongGuesses;
-
-const wordElement = document.getElementById('word');
-const letterCountElement = document.getElementById('letterCount');
-const lettersGuessedElement = document.getElementById('lettersGuessed');
+document.body.onload = initializeGame();
 
 function initializeGame() {
-  const wordList = ['incongruent','effulgent','koinonia','melancholy','vivacious'];
-  Array.prototype.sample = function(){
-    return this[Math.floor(Math.random()*this.length)];
-  }
-  word = wordList.sample();
-  allowedGuesses = 7;
-  wrongGuesses = [];
-  correctGuesses = [];
-
+  
+  const wordElement = document.getElementById('word');
+  const letterCountElement = document.getElementById('letterCount');
+  const lettersGuessedElement = document.getElementById('lettersGuessed');
+  
   let img = document.createElement("IMG");
   img.src = 'gallows.jpg'
   let element = document.getElementById("img");
   element.appendChild(img);
 
+  const wordList = ['incongruent', 'effulgent', 'koinonia', 'melancholy', 'vivacious'];
   
-  function updateImg(){
-  let img = document.createElement("IMG");
-  if (allowedGuesses = 6){
-      img.src = 'gallows+head.jpg';}
-    // }else if (allowedGuesses = 6){
-    //   img.src = 'gallows2.jpg';
-    // }
-    let element = document.getElementById("img");
-    element.appendChild(img);
+  Array.prototype.sample = function () {
+    return this[Math.floor(Math.random() * this.length)];
   }
 
-  
+  const word = wordList.sample();
+  let allowedGuesses = 6;
+  let wrongGuesses = [];
+  let correctGuesses = [];
 
-  // initialize correctGuesses array with underscores
   for (let i = 0; i < word.length; i++) {
     correctGuesses.push('_');
   }
 
   wordElement.innerHTML = correctGuesses.join(' ');
   letterCountElement.innerHTML = allowedGuesses;
-}
 
-function updateGuesses(letter) {
-  allowedGuesses--; // decrement guesses left
-  letterCountElement.innerHTML = allowedGuesses;
-  if (allowedGuesses === 0) {
-    alert('You Lost!');
+  function updateImg(x) {
+    if (x === 5) {
+      img.src = 'gallows+head.jpg'
+    };
+    if (x === 4) {
+      img.src = 'gallows+head+torso.jpg'
+    };
+    if (x === 3) {
+      img.src = 'gallows+head+torso+leg.jpg'
+    };
+    if (x === 2) {
+      img.src = 'gallows+head+torso+2leg.jpg'
+    };
+    if (x === 1) {
+      img.src = 'gallows+head+torso+2leg+arm.jpg'
+    };
+    if (x === 0) {
+      img.src = 'gallows+head+torso+2leg+2arm.jpg'
+    };
+
   }
 
-  if (word.indexOf(letter) === -1) { // letter is NOT in the word
-    wrongGuesses.push(letter); // update letters guessed
-    lettersGuessedElement.innerHTML = wrongGuesses.join(', ');
-  } else { // letter IS in the word
-    // replace underscore with the letter
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === letter) {
-        correctGuesses[i] = letter;
+  // initialize correctGuesses array with underscores
+
+  function updateGuesses(letter) {
+    allowedGuesses--; // decrement guesses left
+    letterCountElement.innerHTML = allowedGuesses;
+    updateImg(allowedGuesses);
+    if (allowedGuesses === 0) {
+      alert('You Lost!');
+    }
+
+    if (word.indexOf(letter) === -1) { // letter is NOT in the word
+      wrongGuesses.push(letter); // update letters guessed
+      lettersGuessedElement.innerHTML = wrongGuesses.join(', ');
+    } else { // letter IS in the word
+      // replace underscore with the letter
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+          correctGuesses[i] = letter;
+        }
       }
+
+      wordElement.innerHTML = correctGuesses.join(' ');
     }
-
-    wordElement.innerHTML = correctGuesses.join(' ');
   }
-}
 
-function checkWin() {
-  if (correctGuesses.indexOf('_') === -1) {
-    alert('You Won!');}
-  // } else if (allowedGuesses === 0) {
-  //   alert('You Lost!');
-  // }
-}
+  function checkWin() {
+    if (correctGuesses.indexOf('_') === -1) {
+      alert('You Won!');
+    }
+  }
 
-document.onkeyup = function (event) {
-  let letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-  updateGuesses(letterGuessed);
-  updateImg();
-  checkWin();
-};
+  document.onkeyup = function (event) {
+    let letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    document.getElementById(`${letterGuessed}`).style.background = 'red';
+    updateGuesses(letterGuessed);
+    checkWin();
+  };
 
-initializeGame();
-
-function addBlocks(){
-  for (let i = 10; i < 36; i++) {
-    const block = document.createElement("span");
-    block.id = `${i}`;
-    block.innerHTML = `<span> ${i.toString(36)} </span>`;
-    block.style.color = 'red';
-    block.setAttribute = ('display','inline');
-    block.style.border = "thin solid grey";
-    block.style.borderRadius = ".25rem";
-    block.style.display = "inline-block";
-    block.style.textAlign = "center";
-    block.style.width = "30px"
-
-    function changeColour(){
-      block.style.background = 'red';
+  function addBlocks() {
+    for (let i = 10; i < 36; i++) {
+      const block = document.createElement("span");
+      block.id = `${i.toString(36)}`;
+      block.innerHTML = `<span> ${i.toString(36)} </span>`;
+      block.style.color = 'red';
+      block.setAttribute = ('display', 'inline');
+      block.style.border = "thin solid grey";
+      block.style.borderRadius = ".25rem";
+      block.style.display = "inline-block";
+      block.style.textAlign = "center";
+      block.style.width = "30px"
       
+      function changeColour() {
+        block.style.background = 'red';
+        let letterGuessed = `${i.toString(36)}`;
+        updateGuesses(letterGuessed);
+        checkWin();
+      }
+      block.onclick = changeColour;
+      // block.onmouseover.style.background = "red";
+      
+      const element = document.getElementById("letters")
+      element.appendChild(block);
     }
-    block.onclick = changeColour;
-    // block.onmouseover.style.background = "red";
-    
-    const element = document.getElementById("letters")
-    element.appendChild(block);
   }
+  addBlocks()
 }
